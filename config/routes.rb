4e838a2c43ab490e42ff98fa81a6634c9102e67f
Dong Duc Thuong <dongducthuong@gmai.com>
2018-05-23 22:07:00 +0700

    friendship views

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'search_stocks', to: 'stocks#search'
   resources :user_stocks, only: [:create, :destroy]
   get 'my_friends', to: 'users#my_friends'
-
+  resources :users, only: [:show]
+	resources :friendships
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
